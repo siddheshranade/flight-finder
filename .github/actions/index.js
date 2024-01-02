2006f29eee37e50a7b31166779ec2d8dbbd72c6b
unknown <ranade.siddhesh@gmail.com>
2024-01-02 17:01:18 -0500

    Add octokit to comment on PR

--- .github/actions/index.js
+++ .github/actions/index.js
@@ -1,1 +1,22 @@
-console.log('I should appear when you make a PR! 🎁');
+const { context, getOctokit } = require('@actions/github');
+
+console.log('1. Loaded index.js! 🎁');
+
+async function commentOnPullRequest() {
+    console.log('3. Inside async! 🎁');
+    const token = process.env.GITHUB_TOKEN;
+    const octokit = getOctokit(token);
+
+    const { owner, repo, number } = context.issue;
+
+    await octokit.issues.createComment({
+        owner,
+        repo,
+        issue_number: number,
+        body: 'Testing comment from GitHub Actions! Messi.'
+    });
+    console.log('4. Inside async! 🎁');
+}
+
+console.log('2. Calling async function!! 🎁');
+commentOnPullRequest();
