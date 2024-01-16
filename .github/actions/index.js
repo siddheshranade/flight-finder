edc2784228fa1a0947e336ffed994a7b0a1d4cd5
unknown <ranade.siddhesh@gmail.com>
2024-01-16 13:28:52 -0500

    x2

--- .github/actions/index.js
+++ .github/actions/index.js
@@ -4,10 +4,11 @@ console.log('1. Loaded index.js! 🎁');
 
 const GITHUB_FINE_GRAIN_TOKEN = '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';
 const GITHUB_CLASSIC_TOKEN = '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';
+const GITHUB_CLASSIC_TOKEN_BETTER = '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';
 
 async function commentOnPullRequest() {
     console.log('3. Inside async! 🎁');
-    const githubApiToken = GITHUB_FINE_GRAIN_TOKEN;
+    const githubApiToken = GITHUB_CLASSIC_TOKEN_BETTER;
     // Create a personal access token at https://github.com/settings/tokens/new?scopes=repo
 const octokit = new Octokit({ auth: githubApiToken });
 
@@ -17,12 +18,12 @@ const {
 } = await octokit.rest.users.getAuthenticated();
 console.log("Hello, %s", login);
 
-await octokit.rest.issues.create({
-    owner: "siddheshranade",
-    repo: "flight-finder",
-    title: "Issue from Octokit",
-    body: "I created this issue using Octokit!",
-  });
+// await octokit.rest.issues.create({
+//     owner: "siddheshranade",
+//     repo: "flight-finder",
+//     title: "Issue from Octokit",
+//     body: "I created this issue using Octokit!",
+//   });
 
     // const { owner, repo, number } = context.issue;
 
