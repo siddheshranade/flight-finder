4d10fbca4fcc6172608bc2763d94577039f10ecd
unknown <ranade.siddhesh@gmail.com>
2024-01-17 13:27:32 -0500

    sheets

--- .github/actions/index.js
+++ .github/actions/index.js
@@ -1,15 +1,15 @@
 import { Octokit } from "@octokit/core";
-
+import { google } from "googleapis";
 console.log('1. Loaded index.js! 🎁');
 
-const GITHUB_FINE_GRAIN_TOKEN = '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';
-const GITHUB_CLASSIC_TOKEN = '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'; // created 1st
-const GITHUB_CLASSIC_TOKEN_BETTER = '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'; // created 3rd - WORKS (not that others don't, haven't tested)
+// const GITHUB_FINE_GRAIN_TOKEN = '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';
+// const GITHUB_CLASSIC_TOKEN = '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'; // created 1st
+// const GITHUB_CLASSIC_TOKEN_BETTER = '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'; // created 3rd - WORKS (not that others don't, haven't tested)
 
-const GITHUB_UMD_ACCOUNT_FINE_GRAIN = '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'; // has ALL permissions
-const GITHUB_UMD_ACCOUNT_CLASSIC = 
-    // 'ghp_' +
-    'h4iqaiS5ukQ92QOamLEOO9piw8KzJc3kR3J9'; // has ALL permissions
+// const GITHUB_UMD_ACCOUNT_FINE_GRAIN = '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'; // has ALL permissions
+// const GITHUB_UMD_ACCOUNT_CLASSIC = 
+//     // 'ghp_' +
+//     'h4iqaiS5ukQ92QOamLEOO9piw8KzJc3kR3J9'; // has ALL permissions
 
 async function commentOnPullRequest() {
     console.log('3. Inside async - start! 🎁');
@@ -64,7 +64,37 @@ console.log('RESPONSE ', response);
     console.log('4. Inside async - end! 🎁');
 }
 
+async function getValueFromSheet() {
+    console.log('#3');
+    const sheets = google.sheets('v4');
+
+    const auth = new google.auth.GoogleAuth({
+        keyFile: './cla-checking-test-gcp-service-key.json',
+        scopes: ['https://www.googleapis.com/auth/spreadsheets']
+    });
+    console.log('#4 ', auth);
+
+    const authClient = await auth.getClient();
+    console.log('#5 ', authClient);
+    sheets.spreadsheets.values.get({
+        spreadsheetId: '',
+        range: 'Sheet1!A1',
+        auth: authClient
+    },
+    (err, response) => {
+        if (err) {
+            console.error('The API returned an error:', err);
+            return;
+        }
+
+        const value = response.data.values[0][0];
+        console.log('VALUE ', value);
+    }
+    );
+    console.log('#6 after get');
+}
+
 console.log('2. Calling async function!! 🎁');
+// commentOnPullRequest();
+getValueFromSheet();
 
-
-commentOnPullRequest();
