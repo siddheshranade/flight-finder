2d0b137b3245ac55a60f1cfb34770ce1990b4b55
unknown <ranade.siddhesh@gmail.com>
2024-02-02 15:22:19 -0600

    x

--- .github/scripts/index.js
+++ .github/scripts/index.js
@@ -3,157 +3,170 @@ import { google } from "googleapis";
 import Handlebars from "handlebars";
 import fs from "fs-extra";
 
+const GITHUB_REPOSITORY = process.env.GITHUB_REPOSITORY || "siddheshranade/flight-finder";
+
 const PULL_REQUST_INFO = {
-    id: process.env.PR_NUMBER,
-    repoName: process.env.GITHUB_REPOSITORY.split('/')[1],
-    username: process.env.GITHUB_ACTOR || 'siddheshranade',
-    gitHubToken: process.env.GITHUB_TOKEN
+  id: process.env.PR_NUMBER || 49,
+  owner: GITHUB_REPOSITORY.split("/")[0],
+  repoName: GITHUB_REPOSITORY.split("/")[1],
+  username: process.env.GITHUB_ACTOR,
+  gitHubToken: process.env.GITHUB_TOKEN,
 };
 
-/* TODO: Store in repo secrets */
-/* TODO: Replace with actual Cesium spreadsheet Ids */
+/* TODO: 1. Replace with actual Cesium CLA spreadsheet Ids, 2. Retrieve Ids from GitHub Secrets, not expose */
 const GOOGLE_SHEETS_INFO = {
-    individualCLASheetId: '1oRRS8OG4MfXaQ8uA4uWQWukaOqxEE3N-JuqzrqGGeaE',
-    corporateCLASheetId: '1dnoqifzpXB81G1V4bsVJYM3D19gXuwyVZZ-IgNgCkC8'
+  individualCLASheetId: "1oRRS8OG4MfXaQ8uA4uWQWukaOqxEE3N-JuqzrqGGeaE",
+  corporateCLASheetId: "1dnoqifzpXB81G1V4bsVJYM3D19gXuwyVZZ-IgNgCkC8",
 };
 
 /* TODO: Change to actual link */
 const LINKS = {
-    contributorsListURL: 'https://google.com'
+  contributorsListURL: "https://google.com",
 };
 
 const main = async () => {
-    console.log('--PULL_REQUST_INFO-- ', PULL_REQUST_INFO);
-    let hasSignedCLA;
-    let errorFoundOnCLACheck;
+  console.log("main()");
+  console.log(
+    PULL_REQUST_INFO.repoName,
+    PULL_REQUST_INFO.owner,
+    PULL_REQUST_INFO.username
+  );
 
-    try {
-        hasSignedCLA = await checkIfUserHasSignedAnyCLA();
-    } catch (error) {
-        errorFoundOnCLACheck = error.toString();
-    }
+  let hasSignedCLA;
+  let errorFoundOnCLACheck;
 
-    const response = await postCommentOnPullRequest(hasSignedCLA, errorFoundOnCLACheck);
+  try {
+    hasSignedCLA = await checkIfUserHasSignedAnyCLA();
+  } catch (error) {
+    console.log("ERROR2 ", error);
+    errorFoundOnCLACheck = error.toString();
+  }
+
+  console.log("pre-comment...");
+  const response = await postCommentOnPullRequest(
+    hasSignedCLA,
+    errorFoundOnCLACheck
+  );
+  console.log("post-comment, response: ", response);
 };
 
-const checkIfUserHasSignedAnyCLA = async () => {    
-    const googleSheetsApi = await getGoogleSheetsApiClient();
+const checkIfUserHasSignedAnyCLA = async () => {
+  let foundIndividualCLA = await checkIfIndividualCLAFound();
+  console.log("CLA #1 ", foundIndividualCLA);
+  if (foundIndividualCLA) {
+    return true;
+  }
 
-    let foundIndividualCLA = await checkIfIndividualCLAFound(googleSheetsApi);
-    if (foundIndividualCLA) {
-        return true;
+  let foundCorporateCLA = await checkIfCorporateCLAFound();
+  console.log("CLA #2 ", foundCorporateCLA);
+  return foundCorporateCLA;
+};
+
+const checkIfIndividualCLAFound = async () => {
+  const response = await getValuesFromGoogleSheet(
+    GOOGLE_SHEETS_INFO.individualCLASheetId,
+    "D2:D"
+  );
+
+  const rows = response.data.values;
+  for (let i = 0; i < rows.length; i++) {
+    if (rows[i].length === 0) {
+      continue;
     }
 
-    let foundCorporateCLA = await checkIfCorporateCLAFound(googleSheetsApi);
-    return foundCorporateCLA;
+    const rowUsername = rows[i][0].toLowerCase();
+    if (PULL_REQUST_INFO.username.toLowerCase() === rowUsername) {
+      return true;
+    }
+  }
+
+  return false;
+};
+
+const checkIfCorporateCLAFound = async () => {
+  const response = await getValuesFromGoogleSheet(
+    GOOGLE_SHEETS_INFO.corporateCLASheetId,
+    "H2:H"
+  );
+
+  const rows = response.data.values;
+  for (let i = 0; i < rows.length; i++) {
+    if (rows[i].length === 0) {
+      continue;
+    }
+
+    // We're more lenient with the ScheduleA username check since it's an unformatted text field.
+    let rowScheduleA = rows[i][0].toLowerCase();
+    rowScheduleA = rowScheduleA.replace(/\n/g, " ");
+    const words = rowScheduleA.split(" ");
+
+    for (let j = 0; j < words.length; j++) {
+      // Checking for substrings because many input their
+      // GitHub username as "github.com/username".
+      if (words[j].includes(PULL_REQUST_INFO.username.toLowerCase())) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+};
+
+const getValuesFromGoogleSheet = async (sheetId, cellRanges) => {
+  const googleSheetsApi = await getGoogleSheetsApiClient();
+
+  return googleSheetsApi.spreadsheets.values.get({
+    spreadsheetId: sheetId,
+    range: cellRanges,
+  });
 };
 
 const getGoogleSheetsApiClient = async () => {
-    const auth = new google.auth.GoogleAuth({
-        keyFile: 'GoogleConfig.json',
-        scopes: ['https://www.googleapis.com/auth/spreadsheets']
-    });
-    const googleAuthClient = await auth.getClient();
+  const auth = new google.auth.GoogleAuth({
+    keyFile: "GoogleConfig.json",
+    scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+  });
+  const googleAuthClient = await auth.getClient();
 
-    return google.sheets({version: 'v4', auth: googleAuthClient });
-};
-
-const checkIfIndividualCLAFound = async (googleSheetsApi) => {
-    const response = await googleSheetsApi.spreadsheets.values.get({
-        spreadsheetId: GOOGLE_SHEETS_INFO.individualCLASheetId,
-        range: 'D2:D'
-    });
-
-    const rows = response.data.values;
-    for (let i = 0; i < rows.length; i++) {
-        if(rows[i].length === 0) {
-            continue;
-        }
-
-        const rowUsername = rows[i][0].toLowerCase();
-        if (PULL_REQUST_INFO.username.toLowerCase() === rowUsername) {
-            return true;
-        }
-    }
-
-    return false;
-};
-
-const testMethod = () => {
-    return true;
-}
-
-const checkIfCorporateCLAFound = async (googleSheetsApi) => {
-    const response = await googleSheetsApi.spreadsheets.values.get({
-        spreadsheetId: GOOGLE_SHEETS_INFO.corporateCLASheetId,
-        range: 'H2:H'
-    });
-
-    const rows = response.data.values;
-    for (let i = 0; i < rows.length; i++) {
-        if(rows[i].length === 0) {
-            continue;
-        }
-
-        // We're more lenient with the ScheduleA username check since it's an unformatted text field.
-        let rowScheduleA = rows[i][0].toLowerCase();
-        rowScheduleA = rowScheduleA.replace(/\n/g, ' ');
-        const words = rowScheduleA.split(' ');
-
-        for (let j = 0; j < words.length; j++) {
-            // Checking for substrings because many input their 
-            // GitHub username as "github.com/username".
-            if (words[j].includes(PULL_REQUST_INFO.username.toLowerCase())) {
-                return true;
-            }
-        }
-    }
-
-    return false;
+  return google.sheets({ version: "v4", auth: googleAuthClient });
 };
 
 const postCommentOnPullRequest = async (hasSignedCLA, errorFoundOnCLACheck) => {
-    const octokit = new Octokit();
+  console.log("adding comment...");
 
-    return octokit.request(`POST /repos/${PULL_REQUST_INFO.username}/${PULL_REQUST_INFO.repoName}/issues/${PULL_REQUST_INFO.id}/comments`, {
-        owner: PULL_REQUST_INFO.username,
-        repo: PULL_REQUST_INFO.repoName,
-        issue_number: PULL_REQUST_INFO.id,
-        body: getCommentBody(hasSignedCLA, errorFoundOnCLACheck),
-        headers: {
-            authorization: `bearer ${PULL_REQUST_INFO.gitHubToken}`,
-            accept: 'application/vnd.github+json',    
-            'X-GitHub-Api-Version': '2022-11-28'
-        }
-    });
+  const octokit = new Octokit();
+  return octokit.request(
+    `POST /repos/${PULL_REQUST_INFO.owner}/${PULL_REQUST_INFO.repoName}/issues/${PULL_REQUST_INFO.id}/comments`,
+    {
+      owner: PULL_REQUST_INFO.username,
+      repo: PULL_REQUST_INFO.repoName,
+      issue_number: PULL_REQUST_INFO.id,
+      body: getCommentBody(hasSignedCLA, errorFoundOnCLACheck),
+      headers: {
+        authorization: `bearer ${PULL_REQUST_INFO.gitHubToken}`,
+        accept: "application/vnd.github+json",
+        "X-GitHub-Api-Version": "2022-11-28",
+      },
+    }
+  );
 };
 
 const getCommentBody = (hasSignedCLA, errorFoundOnCLACheck) => {
-    const commentTemplate = fs.readFileSync('./.github/scripts/templates/pullRequestComment.hbs', 'utf-8');
-    const getTemplate = Handlebars.compile(commentTemplate);
-    const commentBody = getTemplate({ 
-        errorCla: errorFoundOnCLACheck,
-        hasCla: hasSignedCLA,
-        username: PULL_REQUST_INFO.username,
-        contributorsUrl: LINKS.contributorsListURL
-     });
- 
-    return commentBody;
+  console.log("getting comment template...");
+
+  const commentTemplate = fs.readFileSync(
+    "./.github/actions/check-for-CLA/templates/pullRequestComment.hbs",
+    "utf-8"
+  );
+  const getTemplate = Handlebars.compile(commentTemplate);
+  const commentBody = getTemplate({
+    errorCla: errorFoundOnCLACheck,
+    hasCla: hasSignedCLA,
+    username: PULL_REQUST_INFO.username,
+    contributorsUrl: LINKS.contributorsListURL,
+  });
+
+  return commentBody;
 };
 
 main();
-
-// ---
-
-// export { testMethod };
-
-// module.exports = { checkIfIndividualCLAFound };
-
-// export {
-//     main,
-//     checkIfUserHasSignedAnyCLA,
-//     getGoogleSheetsApiClient,
-//     checkIfIndividualCLAFound,
-//     checkIfCorporateCLAFound,
-//     postCommentOnPullRequest
-// };
